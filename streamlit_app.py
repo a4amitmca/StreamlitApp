a108e55331f98c8d692160757465eb204aa64f3b
a4amitmca <4amitmca@gmail.com>
2026-01-07 17:22:32 +0530

    Update streamlit_app.py
    Added Markdown on streamlit page

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,12 @@ import streamlit as st
 
 st.set_page_config(page_title="Bits ML Classification and Models & Metrics")
 st.title("Classification Model and Evaluation matrix")
-
+st.markdown("""This app train six classification model on a single data set and reports the following  metrices:
+-Accurecy
+-AUC Score
+-Precesion
+-Recall
+-F1 Score
+-Mathhews corelation coefficent (MCC)
+ you can upload your owndata set(CSV) or use the simple ** Breast cancer Wisconsin ** dataset (30 features , 550 instances).           
+""")
