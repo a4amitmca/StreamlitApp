55ecc58dbc90648c94a23942d0ef3d7de9a5421e
a4amitmca <4amitmca@gmail.com>
2026-01-07 17:14:25 +0530

    Update streamlit_app.py
    fix import statement issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import sstreamlit as st
+import streamlit as st
 
 st.set_page_confif(page_title="Bits ML Classification and Models & Metrics")
 st.title("Classification Model and Evaluation matrix")
