378b48c3c892f508abcd2ce61f95b7c2ceea6a1d
a4amitmca <4amitmca@gmail.com>
2026-01-07 17:47:10 +0530

    Update streamlit_app.py
    replace select from choice button

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,8 @@ This app train six classification model on a single data set and reports the fol
 """)
 with st.sidebar:
      st.header("Data & Setting")
-     data_choice=st.radio("choose dataset",["Upload CSV","upload your owndataset"])
-     test_size=st.slider("Test Size (Validation Split)",0.1,0.2,0.02)
+     data_choice=st.selectbox("choose dataset",["Upload CSV","Uploaad Excel CSV supported"])
+     test_size=st.slider("Test Size (Validation Split)",0.1,0.4,0.2,0.02)
      scale_numeric=st.checkbox("Scale Numeric features(StandradScalar)",value=True)
      nb_variant=st.selectbox("Naive Bayes varient",["GaussianNB","MultinomialNB"])
      random_state=st.number_input("Random seed",min_value=0,max_value=10000, value=42,step=1)
