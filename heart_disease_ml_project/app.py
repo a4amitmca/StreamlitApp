4f191c224e8b87351b97244b1e6bf96b4bf9a411
a4amitmca <4amitmca@gmail.com>
2026-01-10 21:01:23 +0530

    Comment out configuration options in app.py
    Comment out test size slider, scaling checkbox, and random state input.

--- heart_disease_ml_project/app.py
+++ heart_disease_ml_project/app.py
@@ -18,10 +18,10 @@ This app train six classification model on a single data set and reports the fol
 with st.sidebar:
      st.header("Data & Setting")
      data_choice=st.selectbox("choose dataset",["Upload CSV","Uploaad Excel CSV supported"])
-     test_size=st.slider("Test Size (Validation Split)",0.1,0.4,0.2,0.02)
-     scale_numeric=st.checkbox("Scale Numeric features(StandradScalar)",value=True)
+     #test_size=st.slider("Test Size (Validation Split)",0.1,0.4,0.2,0.02)
+     #scale_numeric=st.checkbox("Scale Numeric features(StandradScalar)",value=True)
      model_option = st.selectbox("Select Model",["Logistic Regression", "Random Forest", "KNN", "Decision Tree", "SVM", "XGBoost"])
-     random_state=st.number_input("Random seed",min_value=0,max_value=10000, value=42,step=1)
+     #random_state=st.number_input("Random seed",min_value=0,max_value=10000, value=42,step=1)
 if data_choice=="Upload CSV":
      uploaded=st.file_uploader("Upload the CSV file (last colum target recomended)",type=["csv"])
      df=None
