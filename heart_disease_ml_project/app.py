3287f6dbf3ab976ac5d28d2907ae81ab5d91e719
a4amitmca <4amitmca@gmail.com>
2026-01-16 20:59:08 +0530

    Update app.py

--- heart_disease_ml_project/app.py
+++ heart_disease_ml_project/app.py
@@ -23,7 +23,7 @@ with st.sidebar:
      data_choice=st.selectbox("choose dataset",["Upload CSV","Uploaad Excel CSV supported"])
      #test_size=st.slider("Test Size (Validation Split)",0.1,0.4,0.2,0.02)
      #scale_numeric=st.checkbox("Scale Numeric features(StandradScalar)",value=True)
-     model_option = st.selectbox("Select Model",["Logistic Regression", "Random Forest", "KNN", "Decision Tree", "SVM", "XGBoost"])
+     #model_option = st.selectbox("Select Model",["Logistic Regression", "Random Forest", "KNN", "Decision Tree", "SVM", "XGBoost"])
      #random_state=st.number_input("Random seed",min_value=0,max_value=10000, value=42,step=1)
 if data_choice=="Upload CSV":
      uploaded=st.file_uploader("Upload the CSV file (last colum target recomended)",type=["csv"])
@@ -36,36 +36,31 @@ if data_choice=="Upload CSV":
       
 else:
      st.markdown("--------------Default File Heart.csv---------------------------")    
-     csv_url = "https://raw.githubusercontent.com/a4amitmca/StreamlitApp/master/heart_disease_ml_project/data/heart.csv"
+     csv_url = "https://github.com/madhurendrabtech/mlassignment2/blob/main/WA_Fn-UseC-Telco-Customer-Churn.csv"
      df = pd.read_csv(csv_url)
      st.dataframe(df.head())
  
-st.markdown("--------------Matrix for all 6 Model of Heart.csv---------------------------") 
-csv_url="https://raw.githubusercontent.com/a4amitmca/StreamlitApp/master/heart_disease_ml_project/models/metrics.csv"
-df = pd.read_csv(csv_url)
-st.dataframe(df.head())
+st.markdown("--------------Report for all 6 Model of WA_Fn-UseC-Telco-Customer-Churn.csv.csv---------------------------") 
+response = requests.get(""https://github.com/madhurendrabtech/mlassignment2/blob/main/Models/reports.json")
+    if response.status_code == 200:
+        metrics_text = response.text
+        st.subheader(f"Evaluation Metrics for {model_option}")
+        st.markdown(f"```\n{metrics_text}\n```")  # preserves formatting
+    else:
+        st.error(f"Could not fetch the metrics file forWA_Fn-UseC-Telco-Customer-Churn.csv")
 st.markdown("--------------This is Matrix for all model selection of Heart.csv---------------------------") 
 
-metrics_urls = {
-    "Logistic Regression": "https://raw.githubusercontent.com/a4amitmca/StreamlitApp/master/heart_disease_ml_project/models/LogisticRegression_report.txt",
-    "Random Forest":       "https://raw.githubusercontent.com/a4amitmca/StreamlitApp/master/heart_disease_ml_project/models/RandomForest_report.txt",
-    "KNN":                 "https://raw.githubusercontent.com/a4amitmca/StreamlitApp/master/heart_disease_ml_project/models/kNN_report.txt",
-    "Decision Tree":       "https://raw.githubusercontent.com/a4amitmca/StreamlitApp/master/heart_disease_ml_project/models/Decision_Tree_report.txt",
-    "SVM":                 "https://raw.githubusercontent.com/a4amitmca/StreamlitApp/master/heart_disease_ml_project/models/NaiveBayes(Gaussian)_report.txt",
-    "XGBoost":             "https://raw.githubusercontent.com/a4amitmca/StreamlitApp/master/heart_disease_ml_project/models/XGBoost_report.txt",
-}
 
 # Fetch and display metrics
-url = metrics_urls.get(model_option)  # Get URL for selected model
-if url:
-    response = requests.get(url)
+response = requests.get("https://github.com/madhurendrabtech/mlassignment2/blob/main/Models/metrics.json")
     if response.status_code == 200:
         metrics_text = response.text
         st.subheader(f"Evaluation Metrics for {model_option}")
         st.markdown(f"```\n{metrics_text}\n```")  # preserves formatting
     else:
         st.error(f"Could not fetch the metrics file for {model_option}")
-else:
-    st.warning("No metrics available for the selected model")
+
+
+    
          
    
